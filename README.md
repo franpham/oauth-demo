12f307f9dae9e104ca158cf03c3a11aa84ce6904
franpham <franpham@gmail.com>
2015-10-30 16:07:14 -1000

    finished Step 2: set the callback route that's called after authorization; provider returns access code as as query string

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 #
 Demo using node-oauth
+
+##
+Step 1: demo .env file to set process.env values for node; MUST add .env to .gitignore to protect info;
+
+Step 2: use oauth2 to get the provider's auth url;
+
+Step 3: set the callback route that's called after authorization; provider returns access code as as query string;
--- index.js
+++ index.js
@@ -17,7 +17,9 @@ app.get('/', (req, res) => {
   res.json({ status: 200 });
 });
 
-// Step 1: OAuth2: get the provider's auth url;
+// Step 1: demo .env file to set process.env values for node; MUST add .env to .gitignore to protect info;
+
+// Step 2: use oauth2 to get the provider's auth url;
 app.get('/auth/login', (req, res) => {
   var authURL = oauth2.getAuthorizeUrl({
     redirect_uri: 'http://localhost:3000/auth/github/callback',
@@ -25,6 +27,28 @@ app.get('/auth/login', (req, res) => {
     state: 'Authorize' + Math.round(Math.random() * 9999999)
   });
   res.json({ url: authURL });    // state variable prevents cross-site forgery;
+});
+
+// Step 3: set the callback route that's called after authorization; provider returns access code as as query string;
+app.get('/auth/github/callback', (req, res) => {
+  var code = req.query.code;
+  if (!code)
+    return res.status(401).json({ error: 401, message: 'Invalid auth code.' });
+  oauth2.getOAuthAccessToken(code,
+    { redirect_uri: 'http://localhost:3000/auth/github/callback' },
+    (err, access_token, refresh_token, results) => {
+      if (err) {
+        console.error(err);
+        res.status(401).json(err);
+      }
+      else if (results.error) {
+        console.error(results.error);
+        res.status(401).json(results.error);
+      }
+      else {  // send token back to the client;
+        res.json({ access_token: access_token });
+      }
+    });
 });
 
 app.listen(PORT, () => {
